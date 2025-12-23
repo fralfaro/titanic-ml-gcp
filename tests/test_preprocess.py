b4b18b76c120c03cbf4302554eb4506319196ed5
fralfaro <francisco.alfaro.496@gmail.com>
2025-12-22 21:26:59 -0300

    ingesta y preprocesamiento de datos

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,2 +1,9 @@
-def test_dummy():
-    assert True
+from titanic_ml.data.preprocess import load_data, preprocess
+
+
+def test_preprocess_returns_dataframe():
+    df = load_data()
+    df_processed = preprocess(df)
+
+    assert "Survived" in df_processed.columns
+    assert df_processed.isnull().sum().sum() == 0
